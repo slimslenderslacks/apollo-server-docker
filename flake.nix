5362e2408d28ef9d17219e95160dc7baf316e81c
Jim Clark <slimslenderslacks@gmail.com>
2023-07-18 16:01:22 -0700

    sync with docs

--- flake.nix
+++ flake.nix
@@ -4,11 +4,13 @@
     platform-engineering.url = "github:slimslenderslacks/nix-modules";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = { nixpkgs, ... }@inputs:
-    inputs.platform-engineering.node-project
+  outputs = { nixpkgs, platform-engineering }:
+    platform-engineering.node-project
       {
         inherit nixpkgs;
         dir = ./.;
+        name = "apollo-server";
+        version = "0.1.0";
       };
 }
 
